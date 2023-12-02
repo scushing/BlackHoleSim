551bef15cac163a081fd08b781e1fc09985c4f80
Stephen Cushing <73804392+scushing@users.noreply.github.com>
2023-12-01 16:10:58 -0800

    SphereController & RayMarcher

--- BlackHoleSim/Assets/Scripts/RayMarcher.cs
+++ BlackHoleSim/Assets/Scripts/RayMarcher.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Matter : MonoBehaviour
+public class RayMarcher : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
