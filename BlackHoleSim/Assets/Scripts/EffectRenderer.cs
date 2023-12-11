253f6e4e79ca34833aad9af5fdd5d022ea926820
Stephen Cushing <73804392+scushing@users.noreply.github.com>
2023-12-11 12:35:36 -0800

    Code comments and scenes

--- BlackHoleSim/Assets/Scripts/EffectRenderer.cs
+++ BlackHoleSim/Assets/Scripts/EffectRenderer.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+/// <summary>
+/// Repaints screen with effects from list of materials
+/// </summary>
 [RequireComponent(typeof(Camera))]
 [ExecuteInEditMode]
 [ImageEffectAllowedInSceneView]
 public class MyEffectRenderer : MonoBehaviour
 {
+    // Taking list allows multiple effects to be rendered
     public List<Material> materials;
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         if (materials != null && materials.Count != 0)
         {
+            // Placeholder as screen is repainted
             RenderTexture tmp = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
             foreach (Material material in materials)
             {
+                // Repaints screen with provied effect
                 Graphics.Blit(source, tmp, material);
                 Graphics.Blit(tmp, source);
             }
--- BlackHoleSim/Assets/Scripts/MovingObject.cs
+++ BlackHoleSim/Assets/Scripts/MovingObject.cs
@@ -2,30 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Handles Euler updates for position and velocity of objects
+/// </summary>
 public class MovingObject : MonoBehaviour
 {
-    // Start is called before the first frame update
     public ObjectProperties center;
     public Transform f2;
     public const float G = 6.67408e-11f;
     Vector3 position;
-    Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
-    public float mass = 1.0f;
+    Vector3 velocity = new(0.0f, 0.0f, 0.0f);
     void Start()
     {
         position = transform.localPosition;
         velocity = CalculateVelocity(center.transform.localPosition, f2.transform.position, transform.localPosition);
     }
 
-    // perform Euler Update on the object
+    // Perform Euler Update on the object
     void FixedUpdate()
     {
         position += velocity * Time.deltaTime;
-
         transform.localPosition = position;
         
-        // calculate the force on the object
         Vector3 toCenter = (center.transform.localPosition - position);
+        // Newton's Law of Unversal Gravitation and Newton's 2nd Law
         float acceleration_mag = G * center.Mass / Mathf.Pow(Vector3.Magnitude(toCenter), 2);
         Vector3 acceleration = toCenter.normalized * acceleration_mag;
         velocity += acceleration * Time.deltaTime;
@@ -34,14 +34,18 @@ public class MovingObject : MonoBehaviour
     // Initial velocity for Keplerian Orbit
     private Vector3 CalculateVelocity(Vector3 f1, Vector3 f2, Vector3 position)
     {
+        // Normal to elipse circumference at position
         Vector3 elipse_normal = -((f2 - position) + (f1 - position)).normalized;
+        // Tangent to elipse circumference at position
+        // Only setup to work on xz plane
         Vector3 elipse_tangent = new(-elipse_normal.z, 0, elipse_normal.x);
+        // Flips tangent when necessary to ensure speed calculation is correct
         if (Vector3.Dot(elipse_tangent, f1 - position) < 0.0f) {
             elipse_tangent = -elipse_tangent;
         }
-        Debug.Log("Mass: " + center.Mass);
-
+        // Semi-major axis of elipse
         float a = (Vector3.Distance(f1, position) + Vector3.Distance(f2, position)) / 2;
+        // Velocity formula from Kepler's Laws
         float speed = Mathf.Sqrt(G * center.Mass * (2/Vector3.Magnitude(position - f1) - 1/a));
 
         return elipse_tangent * speed;
